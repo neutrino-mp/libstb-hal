e9b6f6c88b7713369a514a6ccb3a15e312ac9f56
Stefan Seyfried <seife@tuxbox-git.slipkontur.de>
2013-10-13 22:43:19 +0200

    lt_debug: use C linkage, allows usage from non-C++ code

--- common/lt_debug.h
+++ common/lt_debug.h
@@ -21,10 +21,16 @@
 #define HAL_DEBUG_RECORD	7
 #define HAL_DEBUG_ALL		((1<<8)-1)
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 extern int debuglevel;
 
 void hal_set_threadname(const char *name);
 void _lt_debug(int facility, const void *, const char *fmt, ...) __attribute__ ((format (printf, 3, 4)));
 void _lt_info(int facility, const void *, const char *fmt, ...)  __attribute__ ((format (printf, 3, 4)));
 void lt_debug_init(void);
+#ifdef __cplusplus
+}
+#endif
 #endif
